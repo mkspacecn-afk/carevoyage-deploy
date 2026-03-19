e5e2df786da974a7b65db95188515f0866e5e7aa
Sun Jing <mkspace.cn@gmail.com>
2026-03-19 09:27:04 +0800

    fix: remove obsolete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// Railway entry point
-const { execSync } = require('child_process');
-
-// Run prisma generate first
-execSync('npx prisma generate', { stdio: 'inherit' });
-
-// Start Next.js
-const nextStart = require('next/dist/bin/next');
